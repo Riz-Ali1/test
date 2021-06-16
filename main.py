c045197a87e3ff6a19efee7595901c51e7213ef8
ARizwanAli <41603039+ARizwanAli@users.noreply.github.com>
2021-06-16 22:45:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtWebEngineWidgets import *
 #merge bot tests
+#comment234
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
